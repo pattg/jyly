041a804db5f5882fe26ccbb0e6bafe60a1104012
Patrik Gärtner <patrik.gaertner@basalt.de>
2024-06-27 22:17:32 +0200

    add firestore adapter

--- auth.ts
+++ auth.ts
@@ -1,6 +1,15 @@
 import NextAuth from "next-auth";
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY!.replace(/\\n/gm, "\n"),
+    }),
+  }),
   providers: [Google],
 });
