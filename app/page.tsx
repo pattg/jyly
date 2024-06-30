ec1f16d101779d49c63635155b345692c7329cde
Patrik Gärtner <patrik.gaertner@basalt.de>
2024-06-30 17:46:17 +0200

    add shadcn and basic layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import UserAvatar from "@/components/UserAvatar";
-import { SignIn } from "@/components/auth/signin-button";
 import { SignOut } from "@/components/auth/signout-button";
 import { auth } from "@/auth";
 
@@ -9,23 +8,21 @@ const Home = async () => {
   if (!session?.user)
     return (
       <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
-          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-yellow-300 bg-gradient-to-b from-purple-200 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:bg-pink-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm sm:flex">
+          <p>
             Welcome to jyly! You can either play a quick round. Or sign in and
             track your progress. Either way, have fun practising!
           </p>
         </div>
-        <SignIn />
       </main>
     );
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <UserAvatar />
-      <p className="fixed left-0 top-0 flex w-full justify-center border-b border-yellow-300 bg-gradient-to-b from-purple-200 pb-6 pt-8 backdrop-blur-2xl dark:border-red-800 dark:bg-pink-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Happy to have you on board...
-      </p>
-      <SignOut />
+      <p>Happy to have you on board</p>
+      <h1 className="text-4xl text-cyan-500">
+        {session.user.name?.split(" ")[0]}
+      </h1>
       <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex"></div>
     </main>
   );
